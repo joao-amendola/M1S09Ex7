1b034bd56ece8acc046a807baa80605ddb4336b3
João Amendola <mx.joaoo@gmail.com>
2022-12-05 15:30:18 -0300

    Resolução do exercício 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { LocalStorageService } from './local-storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'local-storage-app';
+  constructor( private localStorageService: LocalStorageService) {}
+  public email: string = '';
+  public password: string = '';
+  public queryKey: string = '';
+  public removeKey: string = '';
+  public queryResult: string = '';
+  public person = {
+    email: this.email, 
+    password: this.password
+  }
+  
+  public addPerson(){
+    this.person.email = this.email
+    this.person.password = this.password;
+    this.localStorageService.setItem(this.email,  JSON.stringify(this.person))
+  }
+
+  // public getPerson(nameToRemove:string){
+  //   this.queryResult = this.localStorageService.getItem(nameToRemove);
+  // }
+
+  // public deletePerson(nameToDelete:string){
+  //   this.localStorageService.removeItem(nameToDelete);
+  // }
+
+  // public reset(){
+  //   this.localStorageService.clear();
+  // }
 }
